0b630fb67a6e68721e05d80a3a84cac1c44e440d
magsteen <mag.steensland@gmail.com>
2022-09-06 12:28:49 +0200

    oblig 2 approved

--- 2/c.py
+++ 2/c.py
@@ -44,7 +44,7 @@ class CrossEntropyXORModel:
 
 
 model = CrossEntropyXORModel()
-optimizer = torch.optim.SGD([model.W_1, model.W_2, model.b_1, model.b_2], 0.1)
+optimizer = torch.optim.SGD([model.W_1, model.W_2, model.b_1, model.b_2], 1.0)
 for epoch in range(100_000):
     model.loss(x_train, y_train).backward()
     optimizer.step()
@@ -53,7 +53,7 @@ for epoch in range(100_000):
     if epoch % 10000 == 0: print("epoch: ", epoch)
     
 #Print model variables and loss
-print("W_1 = %s, W_2 = %s, b_1 = %s, b_2 = %s, loss = %s" 
+print("W_1 = %s\nW_2 = %s\nb_1 = %s\nb_2 = %s\nloss = %s" 
       % (model.W_1, model.W_2, model.b_1, model.b_2, model.loss(x_train, y_train))
 )
 
@@ -62,9 +62,43 @@ print("0.0 + 1.0 should give 1.0 => %s" % model.f(x_train[1:2, :]))
 print("1.0 + 0.0 should give 1.0 => %s" % model.f(x_train[2:3, :]))
 print("1.0 + 1.0 should give 0.0 => %s" % model.f(x_train[3:4, :]))
 
-plt.plot(x_train, y_train, 'o', label='$(x^{(i)},y^{(i)})$')
-plt.xlabel('input')
-plt.ylabel('output')
-plt.plot(x_train, model.f(x_train).detach(), label='$\\hat y = f(x) = xW+b$')
-plt.legend()
-plt.savefig("./2/c.png")
+fig = plt.figure("Logistic regression: the logical XOR operator")
+
+plot1 = fig.add_subplot(131, projection='3d')
+
+x1_grid, x2_grid = torch.meshgrid(torch.linspace(-0.25, 1.25, 10), torch.linspace(-0.25, 1.25, 10))
+h1_grid = torch.empty([10, 10])
+h2_grid = torch.empty([10, 10])
+
+for i in range(0, x1_grid.shape[0]):
+    for j in range(0, x1_grid.shape[1]):
+        h = model.f_1(torch.stack([x1_grid[i, j], x2_grid[i, j]]))
+        h1_grid[i, j] = h[0, 1]
+        h2_grid[i, j] = h[0, 0]
+
+plot1_h1 = plot1.plot_wireframe(
+    x1_grid.detach().numpy(),
+    x2_grid.detach().numpy(),
+    h1_grid.detach().numpy(), 
+    color="magenta")
+plot1_h2 = plot1.plot_wireframe(
+    x1_grid.detach().numpy(), 
+    x2_grid.detach().numpy(), 
+    h2_grid.detach().numpy(), 
+    color="cyan")
+
+
+plot1_info = fig.text(0.15, 0.9, "")
+
+plot1.set_xlabel("$x_1$")
+plot1.set_ylabel("$x_2$")
+plot1.set_zlabel("$h_1,h_2$")
+plot1.legend(loc="upper left")
+plot1.set_xticks([0, 1])
+plot1.set_yticks([0, 1])
+plot1.set_zticks([0, 1])
+plot1.set_xlim(-0.25, 1.25)
+plot1.set_ylim(-0.25, 1.25)
+plot1.set_zlim(-0.25, 1.25)
+
+plt.show()
